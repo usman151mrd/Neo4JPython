76ecf102f820d23ee5a29e26ddc2510de6d36e13
Muhammad Usman <sp18-rcs-027@cuilahore.edu.pk>
2021-04-08 17:09:30 +0500

    revised code

--- Wrapper/TGraphWrapper.py
+++ Wrapper/TGraphWrapper.py
@@ -12,42 +12,35 @@ class Neo4jGraph:
     def retrieve_graph(self):
         query = "MATCH(m:HD)-[r:aka]->(n:HardDisk) return m,r,n LIMIT 25"
         response = self.db.retrieve(query)
-
-       # for node in response:
-        #   print(node)
-        #   print(node['m'].labels)
-        #   print(node['n'].labels)
-            #print(node['r'].nodes[0])
-            #print(node['r'].nodes[1])
-        #     #print(node['r'].type)
-        #     print(node['r'])
-        #     properties = node.data()
-        #     #print(properties['m'])
-        #     #print(properties['r'])
-        #     #print(properties['n'])
         self.to_graph(response)
 
     def to_graph(self, response):
-        g = Graph
         _nodes = dict()
         _edges = dict()
         for node in response:
-            source_node = self.to_tnode((node['r'].nodes[0]))
-            target_node = self.to_tnode((node['r'].nodes[1]))
+            source_node = self.to_tnode(node['r'].nodes[0])
+            target_node = self.to_tnode(node['r'].nodes[1])
             _id = node['r'].id
             _type = node['r'].type
             properties = dict(node['r'])
             relation = self.to_relation(_id, source_node, target_node, _type, properties)
             if source_node.Labels[0] not in _nodes.keys():
                 _nodes[source_node.Labels[0]] = source_node
+            if target_node.Labels[0] not in _nodes.keys():
+                _nodes[target_node.Labels[0]] = source_node
+            key = "{source}-{relation}-{target}".format(source=source_node.Labels[0],relation=_type, target=target_node.Labels[0])
+            _edges[key] = relation
+        self.graph.Nodes = _nodes
+        self.graph.Edges = _edges
 
-    def to_tnode(self, node):
+    def to_tnode(self, _node):
         node = Node()
-        properties = node.data()
-        print(set(node.labels))
-        print(node.id)
-        node.Labels = set(node['n'].labels)
-        node.Id = node['n'].id
+        print("38 : ", _node)
+        print(set(_node.labels))
+        print(_node.id)
+        properties = dict(_node)
+        node.Labels = set(_node.labels)
+        node.Id = _node.id
         node.AoKID = properties['AoKID']
         node.AbstractionLevel = properties['AbstractionLevel']
         node.AgeInMilliseconds = properties['AgeInMilliSeconds']
@@ -80,3 +73,5 @@ if __name__ == '__main__':
     g = Graph()
     n4j = Neo4jGraph(g)
     n4j.retrieve_graph()
+    print("nodes : ", g.Nodes)
+    print("edges : ", g.Edges)
