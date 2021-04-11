0c1dacd12835c8a55e952c1e36e787aab2e6ee6d
M. Usman Sarwar <sp18-rcs-027@cuilahore.edu.pk>
2021-04-11 23:04:15 +0500

    retrieved node by id is completed

--- Wrapper/TGraphWrapper.py
+++ Wrapper/TGraphWrapper.py
@@ -1,5 +1,4 @@
 from Neo4JLayer.Neo4j import Neo4Niha
-from Graph import Graph
 from niha_thrift.ttypes import TGraph
 from TRelationWrapper import Neo4jRelation
 
@@ -13,45 +12,43 @@ class Neo4jGraph:
         self.__relation = Neo4jRelation()
 
     def retrieve_graph(self):
-        query = "MATCH (m)-[r:KNOWS]->(n) RETURN m,r,n LIMIT 25"
+        query = "MATCH (m:TEST3)-[r:isA]->(n:TEST4) RETURN m,r,n LIMIT 25"
         response = self.db.retrieve(query)
-        self.graph = to_graph(response)
+        to_graph(response)
         return self.graph
 
     def create_graph(self):
         _node_ids = set()
         _edge_ids = set()
-        for _rel in self.graph.Edges:
+        for _rel in self.graph.Relation:
             self.__relation.set_relation(_rel)
             _ids = self.__relation.create_relation()
-            sid, rid, tid = _ids[0]['sid'], _ids[0]['rid'], _ids[0]['tid']
+            sid, rid, tid = _ids['sid'], _ids['rid'], _ids['tid']
             _rel.SourceNode.Neo4jID = sid
             _rel.TargetNode.Neo4jID = tid
             _rel.Neo4jID = rid
             _node_ids.update([sid, tid])
             _edge_ids.add(rid)
-        query = "create (g:Graph {name:'graph',nid:" + str(list(_node_ids)) + ",rid:" + str(list(
-            _edge_ids)) + "}) return ID(g) as id"
-        self.db.create(query)
+        query = "create (g:Graph {name:'graph',nid:{0},rid:{1}) return ID(g) as id".format(
+            list(_node_ids), list(_edge_ids))
+        response = self.db.create(query)
+
+    def retrieve_by_id(self, _id):
+        q = "match (g:Graph) where ID(g)={0} return g".format(_id)
+        response = self.db.retrieve(q)
+        properties = response[0].data()
+        rel = []
+        for rid in properties['g']['rid']:
+            r = self.__relation.retrieve_by_id(rid)
+            rel += r
+        print(to_graph(rel))
+        print(properties)
 
 
 if __name__ == '__main__':
-    # Nlist=[]
-    # Rlist=[]
-    # node1=TNode(AoKID='1',Labels={"person1"},Value="4",SystemLevelType="abc",AbstractionLevel="first", Tag="abc", Validity="val",ProcessingTag="ptag",TruthValue={'Key1':'1234','Key2':'125'}, Evaluation="aaa",DateTimeStamp="10 April", AttentionLevel="1", AgeInMilliseconds="700")
-    # node2=TNode(AoKID='2',Labels={"person2"},Value="4",SystemLevelType="abc",AbstractionLevel="first", Tag="abc", Validity="val",ProcessingTag="ptag",TruthValue={'Key1':'1234','Key2':'125'}, Evaluation="aaa",DateTimeStamp="10 April", AttentionLevel="1", AgeInMilliseconds="700")
-    # node3=TNode(AoKID='3',Labels={"person3"},Value="4",SystemLevelType="abc",AbstractionLevel="first", Tag="abc", Validity="val",ProcessingTag="ptag",TruthValue={'Key1':'1234','Key2':'125'}, Evaluation="aaa",DateTimeStamp="10 April", AttentionLevel="1", AgeInMilliseconds="700")
-    # Nlist.append(node1)
-    # Nlist.append(node2)
-    # Nlist.append(node3)
-    # rel1=TRelation(AoKID='4', Labels={"knows"},RelationType="KNOWS",SourceNode=node1,TargetNode=node2,Properties="abc",AttentionLevel="alevel",TruthValue={'RKey1':'1234','RKey2':'125'})
-    # rel2=TRelation(AoKID='5', Labels={"Dontknows"},RelationType="DONTKNOWS",SourceNode=node1,TargetNode=node3,Properties="abc",AttentionLevel="alevel",TruthValue={'RKey1':'1234','RKey2':'125'})
-    # Rlist.append(rel1)
-    # Rlist.append(rel2)
-    # g = TGraph(Nodes=Nlist,Edges=Rlist)
     g = TGraph()
     n4j = Neo4jGraph(g)
-    # n4j.create_graph()
-    g = n4j.retrieve_graph()
-    print("nodes : ", g.Nodes)
-    print("edges : ", g.Relation)
+    # n4j.retrieve_graph()
+    n4j.retrieve_by_id(76)
+    # print("nodes : ", g.Nodes)
+    # print("edges : ", g.Relation)
--- Wrapper/TMemoryChunkWrapper.py
+++ Wrapper/TMemoryChunkWrapper.py
@@ -1,36 +1,29 @@
 from Neo4JLayer.Neo4j import Neo4Niha
-from Graph import Graph
+from niha_thrift.ttypes import TNode, TRelation, TGraph, TMemoryChunk
 from TGraphWrapper import Neo4jGraph
-from Relation import Relation
-from Node import Node
 from TRelationWrapper import Neo4jRelation
-from MemoryChunk import MemoryChunk
 
 
 class Neo4jMemoryChunk:
     def __init__(self, memory_chunk):
         self.memory_chunk = memory_chunk
         self.db = Neo4Niha()
-        self.graph = Relation
-
-    def create_query(self):
-        nodes = []
-        edges = []
-        for node in self.memory_chunk.Graph.Nodes:
-            nodes.append(node.Id)
-        for edge in self.memory_chunk.Graph.Relation:
-            edges.append(edge.Neo4jID)
-        query = "create (n:Graph {name:'Graph',nid:" + nodes + ",rid:" + edges + ",TimeStamp:'" \
-                + self.memory_chunk.TimeStamp + "',Capacity:" + self.memory_chunk.Capacity + ", "
-        query += "AttentionLevel:{0},DecayLevel:{1},Importance:{2},Evaluation:{3}) return ID(n) ".format(
-            self.memory_chunk.AttentionLevel, self.memory_chunk.DecayLevel, self.memory_chunk.Importance,
-            self.memory_chunk.Evaluation)
+        self.__relation = TRelation
+        self.__graph = Neo4jGraph
 
     def create_memory_chunk(self):
-        graph = self.memory_chunk.Graph
+        self.__graph.graph = self.memory_chunk.Graph
+        self.__graph.create_graph()
+        query = "create (n:MemoryChunk {name:'MemoryChunk',TimeStamp:'" \
+                + self.memory_chunk.TimeStamp + "',Capacity:" + self.memory_chunk.Capacity + ", "
+        query += "AttentionLevel:{0},DecayLevel:{1},Importance:{2},Evaluation:{3},gid:{4}) return ID(n) as id ".format(
+            self.memory_chunk.AttentionLevel, self.memory_chunk.DecayLevel, self.memory_chunk.Importance,
+            self.memory_chunk.Evaluation, self.memory_chunk.Graph.Neo4jID)
+        response = self.db.create(query)
+        self.graph.ID = response[0]['id']
 
     def retrieve_memory_chunk(self):
-        graph = Graph()
+        graph = TGraph()
         neo4graph = Neo4jGraph(graph)
         neo4graph.retrieve_graph()
         self.to_memorychunck(graph)
@@ -42,9 +35,9 @@ class Neo4jMemoryChunk:
         pass
 
     def to_memory_chunck(self, neo4graph):
-        memorychunk = MemoryChunk()
-        memorychunk.Graph = neo4graph
+        memory_chunk = TMemoryChunk()
+        memory_chunk.Graph = neo4graph
         # memorychunk.type="graph"
-        memorychunk.Nid = neo4graph.Nodes
-        memorychunk.Rid = neo4graph.Relation
-        memorychunk.rtype = neo4graph.Relation['_relation']
+        memory_chunk.Nid = neo4graph.Nodes
+        memory_chunk.Rid = neo4graph.Relation
+        memory_chunk.rtype = neo4graph.Relation['_relation']
